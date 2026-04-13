b9637b629afddfd00e62d1e99dd6b33a9343b2b5
Felipe Terra <felipe.terra10si@gmail.com>
2026-04-13 19:39:33 -0400

    Feat: Implementada a valicação/exceção dentro do Main (forma muito ruim de fazer esse tratamento).

--- EXERCICIOS/MODULO_14/EXERCICIO_RESOLVIDO_01/src/main/java/Entities/Reservation.java
+++ EXERCICIOS/MODULO_14/EXERCICIO_RESOLVIDO_01/src/main/java/Entities/Reservation.java
@@ -2,47 +2,46 @@ package Entities;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Reservation {
     private Integer roomNumber;
-    private Date checkin;
-    private Date checkout;
+    private Date checkIn;
+    private Date checkOut;
 
-    private final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
     public Reservation (){}
 
-    public Reservation( Integer roomNumber, Date checkin, Date checkout){
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut){
         this.roomNumber = roomNumber;
-        this.checkin = checkin;
-        this.checkout = checkout;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
     public Integer getRoomNumber() { return roomNumber; }
     public void setRoomNumber(Integer roomNumber) { this.roomNumber = roomNumber; }
 
-    public Date getCheckin() { return checkin; }
-    public void setCheckin(Date checkin) { this.checkin = checkin; }
+    public Date getCheckIn() { return checkIn; }
 
-    public Date getCheckout() { return checkout; }
-    public void setCheckout(Date checkout) { this.checkout = checkout; }
+    public Date getCheckOut() { return checkOut; }
 
-    public Integer duration(){
-        Integer days = ;
-        return days;
+    public long duration(){
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
     public void updateDates(Date checkin, Date checkout){
-        this.checkin = checkin;
-        this.checkout = checkout;
+        this.checkIn = checkin;
+        this.checkOut = checkout;
     }
 
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
-        sb.append("Reservation: ").append(getRoomNumber())
-                .append(", Check-in: ").append(sdf.format(getCheckin()))
-                .append(", Check-out: ").append(sdf.format(getCheckout()))
+        sb.append("Reservation: Room ").append(getRoomNumber())
+                .append(", Check-in: ").append(sdf.format(getCheckIn()))
+                .append(", Check-out: ").append(sdf.format(getCheckOut()))
                 .append(", ").append(duration()).append(" nights");
         return sb.toString();
     }
